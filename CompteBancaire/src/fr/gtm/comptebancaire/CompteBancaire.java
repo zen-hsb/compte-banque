4464381180772e0d7155a29965833362687ce1f2
zen-hsb <zainab.hassanbay@gmail.com>
2019-09-27 15:52:56 +0200

    Version initiale

---
+++ CompteBancaire/src/fr/gtm/comptebancaire/CompteBancaire.java
@@ -0,0 +1,43 @@
+package fr.gtm.comptebancaire;
+
+public class CompteBancaire {
+	public int validationCompte() {
+		
+		int numBanque;
+		int codeEtab;
+		int numCompte;
+		int rib;
+		int longueur;
+		//int concat = (numBanque+codeEtab+numCompte+rib);
+		
+		
+		for(numBanque = 0 ; numBanque <= 5 ; numBanque++) {
+			return numBanque;			
+		}
+			char i = (char) (numBanque);
+		
+		for(codeEtab = 0 ; codeEtab <= 5 ; codeEtab++) {
+			return codeEtab;
+		}
+			char j = (char) (codeEtab);
+			
+		for(numCompte = 0 ; numCompte <= 11 ; numCompte++) {
+			return(numCompte);
+		}
+			while (longueur == 11) {	//ajouter un zéro si la longueur est égale à zéro.
+				return;
+			}
+			char k = (char) (numCompte);
+		
+		for(rib = 0 ; rib <= 2 ; rib++) {
+			return(rib);
+		}
+			char l = (char) (rib);
+		
+			
+		
+		
+					
+		
+	}
+}
---
+++ CompteBancaire/src/fr/gtm/comptebancaire/CompteBancaireTest.java
@@ -0,0 +1,30 @@
+package fr.gtm.comptebancaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompteBancaireTest {
+
+	@Test
+	public void testBanque() {
+		System.out.println("numéro de banque validé");
+		CompteBancaire compte = new CompteBancaire();
+		
+			
+		}
+	
+	
+	public void testCodeEtab() {
+		System.out.println("code établissement validé");
+	}
+
+	public void testNumCompte() {
+		System.out.println("numéro de compte validé");
+	}
+	
+	public void testRib() {
+		System.out.println("rib validé");
+	}
+	
+}
